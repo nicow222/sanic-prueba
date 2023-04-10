59abe8b6ec42476b78defd30fd3348459b1993c7
Angelo Nicolas Wolhein <angelonicolasw@gmail.com>
2023-04-10 14:17:32 -0300

    rabbit host docker

--- config/rabbitmq.py
+++ config/rabbitmq.py
@@ -1,9 +1,12 @@
 from aio_pika import connect_robust, Message
+import os
+
+rabbitmq_host = os.environ.get('rabbitmq', '127.0.0.1')
 
 async def publish_rabbitmq(queue,body_message):
     # Conectar al servidor RabbitMQ
     connection = await connect_robust(
-        "amqp://guest:guest@127.0.0.1/",
+        f"amqp://guest:guest@{rabbitmq_host}/",
     )
     async with connection:
         channel = await connection.channel()
